ea62ec6fefc4d7d7a62e8d22c2a66757cb08fbab
swaraj-jain <happy2000jain@gmail.com>
2021-05-17 08:39:08 +0530

    req done except OTP ver and fb post and donation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
 from functools import wraps
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-##from flask_mail import Mail, Message
+from flask_mail import Mail, Message
 from random import randint
 import requests
 import time
@@ -30,19 +30,19 @@ db = firebase.database()
 
 app = Flask(__name__)
 
-'''
-for mail
+
 
 mail = Mail(app)
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
-app.config["MAIL_USERNAME"] = "swarajxxx69@gmail.com"
-app.config['MAIL_PASSWORD'] = 'swarajfucks'
+app.config["MAIL_USERNAME"] = "div143har@gmail.com"
+app.config['MAIL_PASSWORD'] = 'Div@143@Har'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
+admin_number="8239335509"
 
-'''
+
 
 
 
@@ -74,9 +74,11 @@ class loginForm(Form):
 def index():
     print(session)
     this_User=""
+    this_User_num=""
     if 'logged_in' in session: 
         this_User =session['username']
-    return render_template("index.html", this_User = this_User)
+        this_User_num=session['mob_num']
+    return render_template("index.html", this_User = this_User , this_User_num=this_User_num ,admin_number=admin_number)
 
 
 
@@ -92,6 +94,22 @@ def is_logged_in(f):
         else:
             flash('Unauthorized, Please login', 'danger')
             return redirect(url_for('login'))
+
+    return wrap
+
+
+###################################################################################################
+
+
+# Check if the currentuser is admin
+def is_admin(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session and session['is_admin']:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized', 'danger')
+            return redirect(url_for('index'))
 
     return wrap
 
@@ -119,7 +137,8 @@ def signup():
         data = {
             "name": name,
             "mob_num": mob_num,
-            "password": password
+            "password": password,
+            "raised_requests":""
         }
         db.child("Users").push(data)
 
@@ -166,6 +185,8 @@ def login():
             session['mob_num'] = user['mob_num']
             session['user_id'] = user_id
             session['is_admin']  = False
+            if mob_num == admin_number:
+                session['is_admin']  = True
             flash('Welcome ' + user['name'] + '!', 'success')
             print(session)
             return redirect(url_for('index'))
@@ -186,8 +207,21 @@ def become_volunteer():
         email = f_data['email']
         city = f_data['city']
         Profession = f_data['Profession']
-        mob_per = f_data['mob_per']
-        email_per = f_data['email_per']
+
+        Volunteers = db.child("Volunteers").get().val()
+
+        for x in Volunteers:
+            if Volunteers[x]['mob_num'] == mob_num:
+                flash('Volunteers with this Number allready exist', 'danger')
+                return redirect(url_for("index"))
+
+        mob_per = "0"
+        email_per = "0"
+ 
+        if (f_data['mob_per']).upper() == "YES":  
+            mob_per="1"
+        if (f_data['email_per']).upper() == "YES":  
+            email_per="1"
 
         data = {
             "name": name,
@@ -198,7 +232,7 @@ def become_volunteer():
             "mob_per" : mob_per,
             "email_per":email_per
         }
-
+        print("hiii")
         db.child("Volunteers").push(data)
 
         flash('you are now a Volunteers and can help other', 'success')
@@ -206,6 +240,78 @@ def become_volunteer():
         return redirect(url_for("index"))
 
     return render_template("be_vol.html")
+
+################################################################################################# Rasing Request
+
+@app.route('/raise_request', methods=['GET','POST'])
+@is_logged_in
+def raise_request():
+    if request.method == 'POST':
+        f_data = request.form
+        issue = f_data['issue']
+        city = f_data['city']
+        issue_subject=f_data['issue_subject']
+        today = datetime.datetime.now()
+        t_date = today.strftime("%d") + "/" + today.strftime("%m") + "/" + today.strftime("%Y")
+        p_time = today.strftime("%H") + ":" + today.strftime("%M") + ":" + today.strftime("%S")
+
+        data = {
+            "city": city,
+            "issue": issue,
+            "issue_subject":issue_subject,
+            "date": t_date
+        }
+        admin_mail="div143har@gmail.com"
+        msg = Message(str(issue_subject) + " - " + str(city), sender='div143har@gmail.com', recipients=[admin_mail])
+        msg.body = str(issue +'\r\n \r\n'+"By :- "+str(session['username'])+" \r\n ("+str(session['mob_num'])+")\r\n City :- "+str(city) )
+        mail.send(msg)
+
+        db.child("Users/"+session['user_id']+"/raised_requests").push(data)
+
+        flash('you Issue has been raised', 'success')
+
+        return redirect(url_for("index"))
+
+    return render_template("raise_request.html")
+
+####################################################################################### My raised request
+
+@app.route('/my_raised_request')
+@is_logged_in
+def my_raised_request():
+    this_User=""
+    this_User_num=""
+    if 'logged_in' in session: 
+        this_User =session['username']
+        this_User_num=session['mob_num']
+    issues = db.child("Users/"+session['user_id']+"/raised_requests").get().val()
+    return render_template("my_raised_request.html", this_User = this_User  , this_User_num=this_User_num, admin_number=admin_number ,issues=issues)
+
+
+####################################################################################### Delet my raised request
+
+@app.route("/delete_raised_request" + "/<req_id>", methods=['POST'])
+def delete_raised_request(req_id):
+    print(req_id)
+    db.child("Users/" + session['user_id']+"/raised_requests/"+req_id).remove()
+    flash('Raised Request Deleted', 'success')
+    return redirect(url_for('my_raised_request'))
+
+
+
+######################################################################################### volunteer list
+
+@app.route('/volunteer_list')
+
+def volunteer_list():
+    this_User=""
+    this_User_num=""
+    if 'logged_in' in session: 
+        this_User =session['username']
+        this_User_num=session['mob_num']
+    v_info = db.child("Volunteers").get().val()
+    return render_template("volunteer_list.html", this_User = this_User , session = session ,v_info=v_info ,this_User_num=this_User_num,admin_number=admin_number )
+
 
 
 
@@ -218,6 +324,30 @@ def logout():
     flash('You are now logged out', 'success')
     return redirect(url_for("index"))
 
+####################################################  Only for  admin
+
+#########################################  ALL RAISED ISSUE
+
+@app.route('/pending_raised_issue')
+@is_admin
+def pending_raised_issue():
+    this_User=""
+    this_User_num=""
+    if 'logged_in' in session:
+        this_User =session['username']
+        this_User_num=session['mob_num']
+    users = db.child("Users").get().val()
+    return render_template("pending_raised_issue.html", this_User=this_User , users=users ,this_User_num=this_User_num ,admin_number=admin_number)
+
+####################################### Remove Volunteer
+
+@app.route("/remove_volunteer" + "/<req_id>", methods=['POST'])
+@is_admin
+def remove_volunteer(req_id):
+    name = db.child("Volunteers/" +req_id+ "/name").get().val()
+    db.child("Volunteers/" +req_id).remove()
+    flash(name+ 'has been removed from Volunteers', 'success')
+    return redirect(url_for('volunteer_list'))
 
 
 #######################################################################################################
